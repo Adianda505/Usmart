959e48d75a20cdfcce9ac3779d73cf0ce3ee640c
Adianda505 <adiazhar38@gmail.com>
2026-04-12 18:05:59 +0700

    fix title footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,9 +1,8 @@
 <footer class="bg-neutral-primary-soft rounded-base shadow-xs border border-default m-4">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-7" alt="UsMart" />
-                <span class="text-heading self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
+            <a href="#" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <span class="text-heading self-center text-2xl font-semibold whitespace-nowrap">Usmart</span>
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-body sm:mb-0">
                 <li>
@@ -23,6 +22,6 @@
             </ul>
         </div>
         <hr class="my-6 border-default sm:mx-auto lg:my-8" />
-        <span class="block text-sm text-body sm:text-center">© 2023 <a href="https://flowbite.com/" class="hover:underline">UsMart™</a>. All Rights Reserved.</span>
+        <span class="block text-sm text-body sm:text-center">© 2023 <a href="#" class="hover:underline">UsMart™</a>. All Rights Reserved.</span>
     </div>
 </footer>
