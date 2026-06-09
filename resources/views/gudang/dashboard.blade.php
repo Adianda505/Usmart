90407b0305902b7deb3eca19e9c3de3bd2a3759d
Adianda505 <adiazhar38@gmail.com>
2026-06-09 21:58:02 +0700

    Menambahkan view untuk dashboard gudang

---
+++ resources/views/gudang/dashboard.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+
+    <x-slot name="header">
+        <div class="flex justify-between items-center">
+
+            <h2 class="font-semibold text-xl text-gray-800">
+                Dashboard Gudang
+            </h2>
+
+        </div>
+    </x-slot>
+
+    <div class="p-6">
+
+        <div class="bg-white p-6 rounded shadow">
+
+            <h1 class="text-2xl font-bold mb-5">
+                Selamat Datang Gudang
+            </h1>
+        </div>
+    </div>
+</x-app-layout>
